316108c160f0d12821f30e284e7ece9144a01eb2
Eduardo Rabelo <oieduardorabelo@gmail.com>
2019-10-06 13:10:05 +1300

    Switch Navigator: app, loading e auth

--- App.js
+++ App.js
@@ -1,19 +1,25 @@
+// App.js
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  createAppContainer,
+  createSwitchNavigator,
+} from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createDrawerNavigator } from 'react-navigation-drawer';
+import { createStackNavigator } from 'react-navigation-stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+import Example from './Example';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const App = createSwitchNavigator({
+  Loading: {
+    screen: Example,
+  },
+  Auth: {
+    screen: Example,
+  },
+  App: {
+    screen: Example,
   },
 });
+
+export default createAppContainer(App);
