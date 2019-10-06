14e6c17d61c5b35c8b3edc137614866b710a2552
Eduardo Rabelo <oieduardorabelo@gmail.com>
2019-10-06 13:10:52 +1300

    Stack Navigator de autenticação: adicionando auth stack

--- App.js
+++ App.js
@@ -10,12 +10,45 @@ import { createStackNavigator } from 'react-navigation-stack';
 
 import Example from './Example';
 
+const AuthStack = createStackNavigator({
+  Landing: {
+    screen: Example,
+    navigationOptions: {
+      headerTitle: 'Landing',
+    },
+  },
+  SignIn: {
+    screen: Example,
+    navigationOptions: {
+      headerTitle: 'Sign In',
+    },
+  },
+  CreateAccount: {
+    screen: Example,
+    navigationOptions: {
+      headerTitle: 'Create Account',
+    },
+  },
+  ForgotPassword: {
+    screen: Example,
+    navigationOptions: {
+      headerTitle: 'Forgot Password',
+    },
+  },
+  ResetPassword: {
+    screen: Example,
+    navigationOptions: {
+      headerTitle: 'Reset Password',
+    },
+  },
+});
+
 const App = createSwitchNavigator({
   Loading: {
     screen: Example,
   },
   Auth: {
-    screen: Example,
+    screen: AuthStack,
   },
   App: {
     screen: Example,
